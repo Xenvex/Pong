916a12ad5d8ecd5a5b899f43d2efbaba4b0bcdcd
Xenvex <matthewbreyes@gmail.com>
2020-09-05 18:02:40 -0700

    Trinary

--- Pong/Assets/Ball.cs
+++ Pong/Assets/Ball.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private Rigidbody rb;
-    private float amplify = 20;
-    
+    private float ampInitial = 50;
+    private float amp = 20;
+    private float embark = -1;
+
+    public Vector3 originalPos;
+
     //private float direction = 1;
-    //random comment here, don't mine me
 
     // Start is called before the first frame update
     void Start()
     {
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
         rb = GetComponent<Rigidbody>();
         //rb = transform.GetComponent<Rigidbody>(); same as the line above
-        AddForce(-1);
+        AddForce(embark);
     }
 
     // Update is called once per frame
@@ -26,42 +30,43 @@ public class Ball : MonoBehaviour
 
     void FixedUpdate()
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-            
-        //}
-        //rb.AddExplosionForce(this.Vector3.right * amplify);
+        //rb.AddExplosionForce(this.Vector3.right * amp);
     }
 
     private void AddForce(float direction)
     {
-        rb.AddForce(new Vector3(1 * direction, 0, 0) * amplify);
+        rb.AddForce(new Vector3(ampInitial * direction, 0, 10));
     }
 
     void OnCollisionEnter(Collision other)
     {
-        //Increase speed on bounce
-        
-
-
         Debug.Log("I hit a " + other.gameObject.name);
-        //other.gameObject.GetComponent<MeshRenderer>().material.color = NewColor();
 
         //Change color of the ball
         gameObject.GetComponent<MeshRenderer>().material.color = NewColor();
+
+        
 
     }
 
     void OnCollisionExit(Collision other)
     {
-        //other.gameObject.GetComponent<MeshRenderer>().material.color = NewColor();
-        amplify += 1000;
+        amp += 1;
+        embark = embark * -1;
+        //rb.velocity = new Vector2((amp-=1) * embark, 0);
+
     }
 
     private Color NewColor()
     {
         Color color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
         return color;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("I scored in the goal!");
+        gameObject.transform.position = originalPos;
     }
 
 
